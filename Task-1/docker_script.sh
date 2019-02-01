43ca5cb0d5e634d0e62e713b9c0ecdc663d39884
David Carl <David.martin.carl@gmail.com>
2019-02-01 23:04:19 +0100

    close #9 update task 1 readme md

--- Task-1/docker_script.sh
+++ Task-1/docker_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 touch docker.db
-docker run -v $PWD/docker.db/:/home/simple.db davidcarl/database_task_1:latest python ./main.py "$@"
+docker run -v $PWD/docker.db/:/home/simple.db davidcarl/database_course:Task-1 python ./main.py "$@"
