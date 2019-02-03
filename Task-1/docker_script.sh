50b2d598902505d65e7d458fc425468d8e8f9824
David Carl <David.martin.carl@gmail.com>
2019-02-03 14:15:14 +0100

    close #12 last fix

--- Task-1/docker_script.sh
+++ Task-1/docker_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 touch docker.db
-docker run -v $PWD/docker.db/:/home/simple.db davidcarl/database_course:Task-1 python ./main.py "$@"
+docker run -v $PWD/simple.db:/home/simple.db davidcarl/database_course:Task-1 python /home/main.py "$@"
