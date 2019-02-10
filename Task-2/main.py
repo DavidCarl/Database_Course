38188348c60ad57a69d4b3339e8cc7e06762c15f
David Carl <David.martin.carl@gmail.com>
2019-02-10 14:33:27 +0100

    close #13 make task2

---
+++ Task-2/main.py
@@ -0,0 +1,72 @@
+import pymongo
+from pymongo import MongoClient
+from bson.son import SON
+from pprint import pprint
+
+
+client = MongoClient()
+db = client.tweet
+training = db.training
+
+def fprint(data):
+    for each in data:
+        pprint(each)
+
+def question1():
+    print('Question 1')
+    data = training.distinct('user')
+    print('Amount of unique users:', len(data))
+
+def question2():
+    print('\nQuestion 2')
+    pipeline = [
+        {"$match": {"text": {"$regex": "@"}}},
+        {"$group": {"_id": "$user", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", -1), ("user", -1)])},
+        {"$limit": 10}
+    ]
+    pprint(list(db.training.aggregate(pipeline)))
+
+def question3():
+    print('\nQuestion 3')
+    print('Got no answer for this question!')
+
+def question4():
+    print('\nQuestion 4')
+    pipeline = [
+        {"$group": {"_id": "$user", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", -1), ("user", -1)])},
+        {"$limit": 10}
+    ]
+    pprint(list(db.training.aggregate(pipeline, session=None)))
+
+
+def question5():
+    print('\nQuestion 5')
+    print('Top 5 negative:')
+    pipeline = [
+        {"$match": {"polarity": 0}},
+        {"$group": {"_id": "$user", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", -1), ("user", -1)])},
+        {"$limit": 5}
+    ]
+    rs = db.training.aggregate(pipeline)
+    fprint(rs)
+    print('\n')
+
+    print('Top 5 positive:')
+    pipeline = [
+        {"$match": {"polarity": 4}},
+        {"$group": {"_id": "$user", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", -1), ("user", -1)])},
+        {"$limit": 5}
+    ]
+    rs = db.training.aggregate(pipeline)
+    fprint(rs)
+
+    
+question1()
+question2()
+question3()
+question4()
+question5()
---
+++ Task-2/readme.md
@@ -0,0 +1,32 @@
+# Task-2
+
+### Made by David Carl
+### www.dcarl.me
+
+## Disclaimer
+
+This is made on a unix based system and made for a unix based system, I cannot offer support for anything else. If you dont have a unix based system, I recommend you strongly to get one! Either on a virtual machine, a VPS or even dual boot / full linux install. I can recommend Ubuntu as a good start.
+
+## Installation
+
+### Local
+
+I would do it this way to run it on you local computer.
+
+```wget https://github.com/DavidCarl/Database_Course/raw/master/Task-2/Task-2.zip```
+
+use your favorite linux zip tool! I use the following
+
+```unzip Task-2.zip```
+
+See usage for how to use it!
+
+## Usage
+
+First off you have to install dependencies. I included a file that dictates which dependencies you need.
+
+You need to run ```pip install -r requirements.txt```
+
+Then you need to run ```python main.py```
+
+This should run the script and print out the results.
